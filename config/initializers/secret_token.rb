05d6a697655d4ede2c42a2f41530c2f356104009
llby <llby@shingor.net>
2014-03-05 00:44:12 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Llysis::Application.config.secret_key_base = '013a8802d3a40472701f1a2ad99d81efd331dd74b5b0f532cda37c0f6f6982d9bce7e38a1517334d25b2d98f4b9cd161f8350da9d18afe3560fb8fd29476183b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Llysis::Application.config.session_store :cookie_store, key: '_llysis_session'
